a8b116496e2658c658c475bdd8a40d08cf428587
Sayed Mohamed <altenenaccs@gmail.com>
2023-12-22 16:18:32 +0300

    -

--- createPoll.php
+++ createPoll.php
@@ -3,56 +3,69 @@
 include "./init/header.php";
 
 ?>
-<?php
 
-include "./init/header.php";
-
-?>
 
 <?php
-
-$db = new PDO('mysql:host=localhost;dbname=pollmaker;charset=utf8', 'root', '');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+$created = null;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $question = $_POST["question"];
     $options = $_POST["op"];
-    $endDate = $_POST["enddate"];
-    $status = (strtotime($endDate) >= strtotime(date("Y-m-d"))) ? 1 : 0;
-    $creationDate = date("Y-m-d");
+  
+    if (empty($question) || empty($options)) {
+  
+      $error = "please fill all the missing blanks !!";
 
-    try {
-        $db->beginTransaction();
-
-        $signedInUserId = $_SESSION["user_id"];
-
-        $insertPollQuery = "INSERT INTO polls (uid, question, status, qdate, edate)
-                            VALUES (:uid, :question, :status, :qdate, :endDate)";
-        $stmt = $db->prepare($insertPollQuery);
-        $stmt->bindParam(':uid', $signedInUserId);
-        $stmt->bindParam(':question', $question);
-        $stmt->bindParam(':status', $status);
-        $stmt->bindParam(':qdate', $creationDate);
-        $stmt->bindParam(':endDate', $endDate);
-        $stmt->execute();
-
-        $questionId = $db->lastInsertId();
-
-        $insertOptionQuery = "INSERT INTO choices (qid, choice) VALUES (:questionId, :option)";
-        $stmt = $db->prepare($insertOptionQuery);
-        $stmt->bindParam(':questionId', $questionId);
-        $stmt->bindParam(':option', $option);
-
-        foreach ($options as $option) {
-            $stmt->execute();
-        }
-
-        $db->commit();
-        echo "Poll created successfully!";
-    } catch (PDOException $e) {
-        $db->rollBack();
-        echo "Error creating poll: " . $e->getMessage();
     }
+ 
+
+  $endDate = $_POST["enddate"];
+  if ($endDate == null) {
+    $status = 1;
+  } else {
+    $status = (strtotime($endDate) >= strtotime(date("Y-m-d"))) ? 1 : 0;
+  }
+
+  $creationDate = date("Y-m-d");
+
+if(empty($error)){
+  try {
+    $db->beginTransaction();
+
+    $signedInUserId = $_SESSION["user_id"];
+
+    $insertPollQuery = "INSERT INTO polls (uid, question, status, qdate, edate)
+                            VALUES (:uid, :question, :status, :qdate, :endDate)";
+    $stmt = $db->prepare($insertPollQuery);
+    $stmt->bindParam(':uid', $signedInUserId);
+    $stmt->bindParam(':question', $question);
+    $stmt->bindParam(':status', $status);
+    $stmt->bindParam(':qdate', $creationDate);
+    $stmt->bindParam(':endDate', $endDate);
+    $stmt->execute();
+
+    $questionId = $db->lastInsertId();
+
+    $insertOptionQuery = "INSERT INTO choices (qid, choice) VALUES (:questionId, :option)";
+    $stmt = $db->prepare($insertOptionQuery);
+    $stmt->bindParam(':questionId', $questionId);
+    $stmt->bindParam(':option', $option);
+
+    foreach ($options as $option) {
+      if($option != ""){
+        $stmt->execute();
+      }
+     
+    }
+
+    $db->commit();
+    $created = "Poll created successfully!";
+  } catch (PDOException $e) {
+    $db->rollBack();
+    $error = "Error creating poll";
+
+  }
+}
 }
 
 ?>
@@ -109,23 +122,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   color: #4682B4;
   border: 1px solid #4682B4;
   }
+
   @media (max-width:600px) {
+
     input,
-  textarea {
-    height: 24px;
-    width: 200px;
-    max-width: 480px;
-    max-height: 60px;
-    padding: 10px;
-    display: block;
-  }
+    textarea {
+      height: 24px;
+      width: 200px;
+      max-width: 480px;
+      max-height: 60px;
+      padding: 10px;
+      display: block;
+    }
   }
 </style>
 <div class="container">
   <h1 style="color:#ed9168;">Create Poll</h1>
   <form method="POST" id="form-c">
-    <span id="error-m"></span>
-    Question: <textarea name="" name="q" col="20" maxlength="255" rows="3"></textarea><br>
+    <?php
+    if (isset($error)) {
+      echo '   <span id="error-m" class="error">';
+
+      echo $error;
+
+      echo '</span>';
+    }
+
+    if (isset($created)) {
+      echo '<span class="success">';
+      echo $created;
+      echo '</span>';
+    }
+    ?>
+    Question: <textarea name="question" name="q" col="20" maxlength="255" rows="3"></textarea><br>
 
     <div id="pollBlock">
       <span class="title">Options:</span>
@@ -134,7 +163,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       Option 2 <input class="options" type="text" name="op[]" required<br>
     </div>
     End date : <input class="" type="date" name="enddate" <br>
-    <button type="submit">Create Poll</button>
+    <button name="create" type="submit">Create Poll</button>
     <button type="button" onclick="addMoreOp()">Add More Options</button>
   </form>
 </div>
