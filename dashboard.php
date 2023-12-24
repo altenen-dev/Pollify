6cd3045a87ba379550fb53bfd143c85ad54b8413
Anas2XA <151394829+Anas2XA@users.noreply.github.com>
2023-12-24 22:08:31 +0300

    Update dashboard.php        Create a poll with simple steps
    added        Create a poll with simple steps

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,12 @@ include "./init/header.php";
 
 ?>
 
-
+<header>
+        <div class="banner h-100">
+       Create a poll with simple steps
+       
+    </div>
+    </header>
 
 <div class="container">
 
@@ -15,9 +20,7 @@ include "./init/header.php";
         <div class="card-container">
             <div class="poll-container">
 
-
                 <?php
-
                 $sqlgetpoll = $db->query("SELECT * FROM `polls`  LIMIT 6");
                 $totalvotes = 0;
                 while ($getInfo = $sqlgetpoll->fetch()) {
@@ -32,54 +35,35 @@ include "./init/header.php";
                         }
                     } else {
                         if ($status == 0) {
-
                             $error = "the poll is expired or deactivated";
-
                         }
                     }
-
-
-                    ?>
+                ?>
 
                     <div class="card">
                         <div class="card-content">
                             <h2>
                                 <?php echo htmlspecialchars($question); ?>
                             </h2>
-                            <a href="./vote.php?id=<?php echo $qid; ?>"><button class="btn">More details</button></a>
-                            <button href="" class="">view results</button>
+                            <a href="./vote.php?id=<?php echo $qid; ?>"><button class="btn">More Details</button></a>
+                            <a href="results.php?id=<?php echo $qid; ?>"><button class="btn">View Results</button></a>
                             <?php
                             if (isset($error)) {
                                 echo '<br>   <span  style="color:#EC8E23;font-size:12px">';
-
                                 echo $error;
-
                                 echo '</span>';
-
                             }
                             ?>
                             <div id="results">
 
                             </div>
-
-
                         </div>
-
                     </div>
-
-
 
                 <?php } ?>
 
-
-
-
             </div>
-
-
-
         </div>
-
 
     </main>
 
